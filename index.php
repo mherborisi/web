4717002dcda479616fbb5272d02f3647b3d57877
my_w_ps <my_w_ps@mail.ru>
2021-10-21 18:07:59 +0400

    CG index.php new metalit.com

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php echo 'Hello World'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>METANIT.COM</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h2>Введи свои данные:</h2>
+<form action="display.php" method="POST">
+<p>Введите имя: <input type="text" name="firstname" /></p>
+<p>Введите фамилию: <input type="text" name="lastname" /></p>
+<input type="submit" value="Отправить">
+</form>
+</body>
+</html>
+
+<?php
+$name = $_POST["firstname"];
+$surname = $_POST["lastname"];
+echo "Ваше имя: <b>".$name . " " . $surname . "</b>";
+?>

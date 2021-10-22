cdf1cf58580f5e8748cc13325d26ec442b220aa7
my_w_ps <my_w_ps@mail.ru>
2021-10-22 09:04:26 +0400

    add upload in index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
                 <br>
                 <br>
             <form action="upload.php" method="post" enctype="multipart/form-data">
-            Select image to upload:
+            Select image to upload: <br>
                     <input type="file" name="fileToUpload" id="fileToUpload">
                     <input type="submit" value="Upload Image" name="submit">
             </form>

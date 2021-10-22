ca629588e2c4f64c579d532d002c47b41f9bfc60
my_w_ps <my_w_ps@mail.ru>
2021-10-22 14:15:27 +0400

    uploads test

--- upload.php
+++ upload.php
@@ -7,24 +7,17 @@
   
 <body>
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-if (isset($_POST["submit"])) {
-    echo "if work"
-}
-// Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//   if($check !== false) {
-//     echo "File is an image - " . $check["mime"] . ".";
-//     $uploadOk = 1;
-//   } else {
-//     echo "File is not an image.";
-//     $uploadOk = 0;
-//   }
-  
+  $target_path = "uploads/";  
+  $target_path = $target_path.basename( $_FILES['fileToUpload']['name']);   
+
+  if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path))
+    {  
+        echo "File uploaded successfully!";  
+    } 
+else
+    {  
+    echo "Sorry, file not uploaded, please try again!";  
+    }  
 ?> 
   </body>
 </html>

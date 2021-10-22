2dd0f15ef32b94ab753349a422d9b3a9b931d042
my_w_ps <my_w_ps@mail.ru>
2021-10-22 08:57:07 +0400

    submit in index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
         <body>
             <h1 style="text-align:center">DevOps Training Course</h1>
             <form action="input_text.php" method="post">
-            Name: <input type="text" name="name"><br>
+            Name: <input type="text" name="text_input"><br>
+            <input type="submit">
             </form>
         </body>
 </html>
